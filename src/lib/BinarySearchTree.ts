0ceecb8006c9a503ef8cc53fc2919c36a47efaaa
tomz <spirit108@foxmail.com>
2020-06-10 22:46:35 +0800

    搜索树

--- src/lib/BinarySearchTree.ts
+++ src/lib/BinarySearchTree.ts
@@ -3,13 +3,21 @@
  * @Author: tom-z(spirit108@foxmail.com)
  * @Date: 2020-05-30 22:00:22
  * @LastEditors: tom-z(spirit108@foxmail.com)
- * @LastEditTime: 2020-06-02 22:08:02
+ * @LastEditTime: 2020-06-10 00:12:36
  */
+
+
 
 interface treeNode {
   key:number,
   left:treeNode | null
   right:treeNode | null
+}
+interface searchRes {
+  flag:boolean,
+  current:treeNode | null
+  parent:treeNode | null
+  isLeftChild: boolean
 }
 
 class TreeNode implements treeNode {
@@ -27,6 +35,10 @@ interface binarySearchTree {
   preOrder:Function // 先序遍历 
   middleOrder:Function // 中序遍历
   postOrder:Function // 中序遍历
+  max:Function // 最大值
+  min:Function // 最小值
+  isNode:Function // 是否有节点.
+  delete:Function
 }
 class BinarySearchTree implements binarySearchTree {
   public root = null as unknown as treeNode
@@ -63,6 +75,7 @@ class BinarySearchTree implements binarySearchTree {
   private insertOrderArr(key:number, arr:Array<number>) {
     arr.push(key)
   }
+
   // 先序遍历
   public preOrder() {
     this.preOrderFn(this.root, this.insertOrderArr)
@@ -76,13 +89,12 @@ class BinarySearchTree implements binarySearchTree {
       node.right && this.preOrderFn(node.right, handle)
     }
   }
+
   // 中序遍历
   public middleOrder() {
     this.middleOrderFn(this.root, this.insertOrderArr)
     return this.middleArr
   }
-
-
   // 中序递归遍历
   private middleOrderFn(node:treeNode, handle:Function) {
     if (node !== null) {
@@ -91,12 +103,12 @@ class BinarySearchTree implements binarySearchTree {
       node.right && this.middleOrderFn(node.right, handle)
     }
   }
-   // 后序序遍历
-   public postOrder() {
+
+   // 后序遍历
+  public postOrder() {
     this. postOrderFn(this.root, this.insertOrderArr)
     return this.postArr
   }
-
   // 后序递归遍历
   private  postOrderFn(node:treeNode, handle:Function) {
     if (node !== null) {
@@ -106,19 +118,199 @@ class BinarySearchTree implements binarySearchTree {
     }
   }
   
+  // 最大值
+  public max():number {
+    let tree = this.root
+    while (tree.right !== null) {
+      tree = tree.right
+    }
+    return tree.key
+  }
+  // 最小值
+  public min():number {
+    let tree = this.root
+    while (tree.left !== null) {
+      tree = tree.left
+    }
+    return tree.key
+  }
+  // 查找
+  public isNode(key:number):Boolean {
+    let res:searchRes = {
+      flag: false,
+      current: this.root,
+      parent: null,
+      isLeftChild: false
+    }
+    
+    this.searchNode(this.root, key, res)
+    return res.flag
+  }
+
+  // 递归搜索节点
+  private searchNode(node:treeNode|null, key:number, res:searchRes):void {
+    if (node === null) {
+      res.current = null
+      res.flag = false
+      return
+    }
+    
+    if (node.key > key) {
+      res.parent = node
+      res.isLeftChild = true
+      this.searchNode(node.left, key, res)
+    }
+    else if (node.key < key) {
+      res.parent = node
+      res.isLeftChild = false
+      this.searchNode(node.right, key, res)
+    }
+    else if (node.key === key) {
+      res.current = node
+      res.flag = true
+      return
+    }
+  }
+
+  // 删除节点
+  public delete(key:number) {
+    let searchRes:searchRes = {
+      flag: false,
+      current: this.root,
+      parent: null,
+      isLeftChild: false
+    }
+    let res:Boolean = true
+    this.searchNode(this.root, key, searchRes)
+    console.log(searchRes)
+    // 未找到
+    if (!searchRes.flag) {
+      res = false
+      return res
+    }
+    // 删除根节点
+    if (searchRes.parent === null) {
+      // 只有根节点
+      if (
+        searchRes.current
+        && searchRes.current.left === null
+        && searchRes.current.right === null
+      ) {
+        this.root === null
+      } else if (
+        searchRes.current
+        && searchRes.current.left
+        && searchRes.current.right === null
+      ) {
+        // 根节点只有左节点
+        this.root = searchRes.current.left
+      } else if (
+        searchRes.current
+        && searchRes.current.left === null
+        && searchRes.current.right
+      ) {
+        // 根节点只有右节点
+        this.root = searchRes.current.right
+      }
+      return res
+    }
+
+    //  删除叶子节点
+    if (
+      searchRes.current
+      && searchRes.current.left === null
+      && searchRes.current.right === null
+    ) {
+      if (searchRes.isLeftChild && searchRes.parent) {
+        searchRes.parent.left = null
+      } else if (!searchRes.isLeftChild && searchRes.parent) {
+        searchRes.parent.right = null
+      }
+      return res
+    }
+    // 删除只有一个子节点的对象,且为左节点
+    if (
+      searchRes.current
+      && searchRes.current.left
+      && searchRes.current.right === null
+    ) {
+      if (searchRes.isLeftChild && searchRes.parent) {
+        searchRes.parent.left = searchRes.current.left
+      } else if (
+        !searchRes.isLeftChild && searchRes.parent) {
+        searchRes.parent.right = searchRes.current.left
+      }
+      return res
+    }
+    // 删除只有一个子节点的对象,且为右节点
+    if (
+      searchRes.current
+      && searchRes.current.left === null
+      && searchRes.current.right
+    ) {
+      console.log(466)
+      if (searchRes.isLeftChild && searchRes.parent) {
+        searchRes.parent.left = searchRes.current.right
+      } else if (!searchRes.isLeftChild && searchRes.parent) {
+        searchRes.parent.right = searchRes.current.right
+      }
+      return res
+    }
+
+    // 删除有两个子节点时
+    if (
+      searchRes.current
+      && searchRes.current.left
+      && searchRes.current.right
+    ) {
+      console.log(33132)
+      // 找到该子节点的后继,然后把后继替换为该节点
+      this.getAfter(searchRes.current, searchRes.parent, searchRes.isLeftChild)
+    }
+  }
+
+  // 线性查找指定节点的后继
+  private getAfter(node:treeNode, parent:treeNode, isLeftChild:boolean) {
+    let deleteNode = node
+    let currentAfter = deleteNode.right 
+    let afterNode:treeNode = currentAfter as treeNode
+    let parentAfterNode:treeNode = currentAfter as treeNode
+    let index = 0
+    
+    while (currentAfter !== null) {
+      index++
+      parentAfterNode = afterNode
+      afterNode = currentAfter
+      currentAfter = currentAfter.left
+    }
+    if (index > 1) {
+      // 当删除节点的后继节点不是直接右子节点
+      parentAfterNode.right = afterNode.right
+      afterNode.right = deleteNode.right
+    }
+    
+    afterNode.left = deleteNode.left
+    if (isLeftChild) {
+      parent.left = afterNode
+    } else {
+      parent.right = afterNode
+    }
+  }
 }
 
 let bst = new BinarySearchTree()
 
+bst.add(10)
 bst.add(7)
 bst.add(1)
 bst.add(2)
 bst.add(4)
 bst.add(5)
-bst.add(10)
 bst.add(11)
 bst.add(8)
 bst.add(3)
+bst.add(15)
+bst.add(16)
 
 console.log(bst.root)
 
@@ -128,3 +320,14 @@ let bstArr1 = bst.middleOrder()
 console.log(bstArr1)
 let bstArr2 = bst.postOrder()
 console.log(bstArr2)
+
+console.log(bst.max())
+console.log(bst.min())
+
+console.log(bst.isNode(3))
+console.log(bst.isNode(50))
+console.log(bst.isNode(7))
+
+// bst.delete(7)
+// console.log(bst.isNode(7))
+console.log(bst.root)
